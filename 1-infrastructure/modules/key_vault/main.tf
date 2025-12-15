d28573c2ce1f19c5b5437458b52e5522c26b0307
Alexclb0 <alex.espinoza.c@uni.pe>
2025-12-15 18:56:08 -0500

    Update main.tf

--- 1-infrastructure/modules/key_vault/main.tf
+++ 1-infrastructure/modules/key_vault/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "main" {
   tenant_id                   = var.tenant_id
   sku_name                    = "standard"
   soft_delete_retention_days  = 7
-  enable_rbac_authorization   = true
+  rbac_authorization_enabled   = true
   purge_protection_enabled    = false
   public_network_access_enabled = true
 

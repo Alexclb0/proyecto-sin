10b48b30f100dd1adf73473a9fdfaa94ab612fbf
Alexclb0 <alex.espinoza.c@uni.pe>
2025-12-15 22:42:43 -0500

    fix

--- 1-infrastructure/modules/databricks_workspace/main.tf
+++ 1-infrastructure/modules/databricks_workspace/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_databricks_workspace" "main" {
   name                          = var.workspace_name
   resource_group_name           = var.resource_group_name
   location                      = var.location
-  sku                           = "standard"
+  sku                           = "premium"
   managed_resource_group_name   = var.managed_resource_group_name
 }
 

400839fe1106887c659249e9c63a71f3d1843dbf
Alexclb0 <alex.espinoza.c@uni.pe>
2025-12-15 22:29:54 -0500

    Update outputs.tf

--- 1-infrastructure/modules/databricks_config/outputs.tf
+++ 1-infrastructure/modules/databricks_config/outputs.tf
@@ -3,8 +3,3 @@ output "token" {
   value       = databricks_token.main.token_value
   sensitive   = true
 }
-
-output "cluster_id" {
-  description = "Databricks cluster ID"
-  value       = databricks_cluster.main.id
-}
